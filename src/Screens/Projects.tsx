c0519d22b7c99b1b9d0950e93df731eae5d529cc
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-25 20:24:22 -0300

    fix: ajuste de cores card

--- src/Screens/Projects.tsx
+++ src/Screens/Projects.tsx
@@ -4,11 +4,6 @@ export default function Projects() {
   return (
     <div className="min-w-full flex flex-row justify-center mt-5 gap-4 flex-wrap">
         <Card description="descrição teste" link="link" title="Título"></Card>
-        <Card description="descrição teste" link="link" title="Título"></Card>
-        <Card description="descrição teste" link="link" title="Título"></Card>
-        <Card description="descrição teste" link="link" title="Título"></Card>
-        <Card description="descrição teste" link="link" title="Título"></Card>
-        <Card description="descrição teste" link="link" title="Título"></Card>
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ interface cardOptions{
 
 export default function Card({title, description, link}: cardOptions) {
   return (
-    <div className="rounded-br-md bg-linear-to-r text-black from-indigo-500 via-indigo-300 to-indigo-200 w-72 h-72 text-center flex flex-col p-5">
+    <div className="rounded-br-md bg-linear-to-r text-black from-indigo-500 via-indigo-300 to-indigo-200 w-72 h-72 text-center flex flex-col p-5 shadow-md shadow-blue-700">
         
         <div className="my-auto">
             <h1 className="font-bold h-11 text-2xl">{title}</h1>
