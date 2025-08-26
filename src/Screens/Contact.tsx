f45de173ff791e321582394151b1b7c8d49b1b2c
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 11:32:28 -0300

    fix: componentizando bolhas

--- src/Screens/Contact.tsx
+++ src/Screens/Contact.tsx
@@ -1,11 +1,30 @@
-
+import Descriptional from "@/components/Descriptional";
+import PinkBubble from "@/components/PinkBubble";
 
 export default function Contact() {
   return (
-    <div className="flex w-full my-10 justify-center">
-      <div className="size-[21rem] bg-white overflow-hidden bg-linear-to-r from-fuchsia-200 rounded-sm">
-         
-      </div>
+    <div className="flex w-full my-10 flex-row justify-center md:gap-5 relative">
+        <span className="hover:scale-110 transition cursor-pointerw  h-fit">
+         <a href="https://www.linkedin.com/in/edusn/" target="_blank">
+           <Descriptional title="Linkedin" description=""/>
+            <p>Conecte-se comigo</p>
+         </a>
+        </span>
+
+        <PinkBubble/>
+
+        <span className="hover:scale-110 transition cursor-pointer h-fit">
+         <a href="https://github.com/EduardoSantos2231" target="_blank">
+           <Descriptional title="Github" description=""/>
+            <p>Veja meus códigos</p>
+         </a>
+        </span>
+
+        <span className="hover:scale-110 transition cursor-pointer h-fit">
+         <Descriptional title="Email" description=""/>
+         <p>Entre em contato</p>
+        </span>
+      
     </div>
   )
 }
