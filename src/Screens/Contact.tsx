214e6ad2dfeb1f423daabd923cc82b6a6878b514
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 12:02:08 -0300

    add: bolhas de estilização

--- src/Screens/Contact.tsx
+++ src/Screens/Contact.tsx
@@ -1,9 +1,10 @@
 import Descriptional from "@/components/Descriptional";
+import GrayBubble from "@/components/GrayBubble";
 import PinkBubble from "@/components/PinkBubble";
 
 export default function Contact() {
   return (
-    <div className="flex w-full my-10 flex-row justify-center md:gap-5 relative">
+    <div className="flex w-full my-10 flex-col items-center md:justify-center md:gap-5 relative overflow-hidden">
         <span className="hover:scale-110 transition cursor-pointerw  h-fit">
          <a href="https://www.linkedin.com/in/edusn/" target="_blank">
            <Descriptional title="Linkedin" description=""/>
@@ -11,19 +12,21 @@ export default function Contact() {
          </a>
         </span>
 
-        <PinkBubble/>
+        <div className="size-80 bg-radial from-blue-500 opacity-90 to-blue-500 rounded-full float absolute top-10 -left-50 md:-left-44"></div>
 
-        <span className="hover:scale-110 transition cursor-pointer h-fit">
+        <span className="hover:scale-110 transition cursor-pointer h-fit z-1">
          <a href="https://github.com/EduardoSantos2231" target="_blank">
            <Descriptional title="Github" description=""/>
             <p>Veja meus códigos</p>
          </a>
         </span>
 
-        <span className="hover:scale-110 transition cursor-pointer h-fit">
+        <span className="hover:scale-110 transition cursor-pointer h-fit z-1">
          <Descriptional title="Email" description=""/>
          <p>Entre em contato</p>
         </span>
+        <PinkBubble/>
+        <GrayBubble/>
       
     </div>
   )
