44e4e36f0952b08c70b5945db00a43cfbb4718f4
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 20:49:01 -0300

    delete: remoção de botão

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 import profile_pic from "@/assets/first_opt.png"
-import { SiReactos } from "react-icons/si";
 export default function Header() {
   return (
     <div className="flex text-white font-bold font-stretch-condensed md:text-xl py-2">
@@ -8,11 +7,11 @@ export default function Header() {
 
             <div className="mr-auto flex gap-5 items-center">
               <img className="rounded-full w-12 md:w-16" src={profile_pic} alt="" />
-              <h1 className="hidden sm:block underline decoration-blue-600 decoration-double md:text-4xl">Eduardo<SiReactos/></h1>
+              <h1 className="hidden sm:block underline decoration-blue-600 decoration-double md:text-4xl">Eduardo</h1>
             </div>
 
             <nav className="p-2">
-            <ul className="flex gap-4 text-lg">
+            <ul className="flex gap-4 text-xl">
               <li className="hover:underline ease-in hover:decoration-blue-600 hover:scale-110 transition"><Link to={"contact"}>Contato</Link></li>
               <li className="hover:underline ease-in hover:decoration-blue-600 hover:scale-110 transition"><Link to={"about"}>Sobre mim</Link></li>
               <li className="hover:underline ease-in hover:decoration-blue-600 hover:scale-110 transition"><Link to={"projects"}>Projetos</Link></li>
