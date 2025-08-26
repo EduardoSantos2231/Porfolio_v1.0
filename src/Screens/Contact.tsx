51d39dd9a7994e194ec957ad7756e154654f825b
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 09:46:18 -0300

    add: criação card contato

--- src/Screens/Contact.tsx
+++ src/Screens/Contact.tsx
@@ -2,6 +2,10 @@
 
 export default function Contact() {
   return (
-    <div>Contac</div>
+    <div className="flex w-full my-10 justify-center">
+      <div className="size-[21rem] bg-white overflow-hidden bg-linear-to-r from-fuchsia-200 rounded-sm">
+         
+      </div>
+    </div>
   )
 }
