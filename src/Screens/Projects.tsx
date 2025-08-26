9807695c34d34ac8f2a77f1bdbd7fe32a84c20cf
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 20:54:30 -0300

    add: linhas de espaçamento

--- src/Screens/Projects.tsx
+++ src/Screens/Projects.tsx
@@ -5,12 +5,14 @@ import Card from "@/components/Card"
 export default function Projects() {
   return (
     <div className="min-w-full flex flex-row justify-center mt-5 gap-4 flex-wrap">
+
         <Card title="Timer" description="Um projeto que realizei durante a fase de aprendizado de 
         manipulação do DOM e funções nativas do javascript, implementei funções de pausar, continuar e começar o timer." 
         link="https://eduardosantos2231.github.io/Timer/"/>
 
         <Card title="Lista de Tarefas" description="Um projeto clássico e interessante para entender o uso de funcionalidades e uso de hooks no react. Foi um dos primeiros projetos que criei com a biblioteca."
         link="https://eduardosantos2231.github.io/ListaTarefas/"/>
+        
     </div>
   )
 }
