c4b136b813d501e8a1ab77f628d45564d5ecfa71
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-25 21:43:31 -0300

    fix: modificação foto de perfil

--- src/Screens/About.tsx
+++ src/Screens/About.tsx
@@ -2,24 +2,24 @@ import photo_preview from "@/assets/second_opt.png"
 
 export default function About() {
   return (
-    <div className="flex w-full m-4 p-4 flex-col md:justify-between">
+    <div className="flex w-full m-4 p-4 flex-col md:justify-around gap-4 ">
 
         <div className="justify-center flex">
-            <img className="size-40 rounded-full border-3" src={photo_preview} alt="Eduardo sorrindo" />
+            <img className="size-40 md:size-60 rounded-full border-3" src={photo_preview} alt="Eduardo sorrindo" />
         </div>
 
         <div className="flex w-full flex-col">
-            <h1 className="font-bold text-2xl">Quem eu sou?</h1>
+            <h1 className="font-bold text-2xl text-justify">Quem eu sou?</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aperiam molestias consectetur! Unde cupiditate, sed pariatur earum laboriosam exercitationem architecto!</p>
         </div>
 
         <div className="flex w-full flex-col">
-            <h1 className="font-bold text-2xl">Por que programação?</h1>
+            <h1 className="font-bold text-2xl text-justify">Por que programação?</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aperiam molestias consectetur! Unde cupiditate, sed pariatur earum laboriosam exercitationem architecto!</p>
         </div>
 
         <div className="flex w-full flex-col">
-            <h1 className="font-bold text-2xl">Tecnologias e habilidades</h1>
+            <h1 className="font-bold text-2xl text-justify">Tecnologias e habilidades</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aperiam molestias consectetur! Unde cupiditate, sed pariatur earum laboriosam exercitationem architecto!</p>
         </div>
         
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import profile_pic from "../assets/fotoperfi.jpeg"
+import profile_pic from "@/assets/first_opt.png"
 export default function Header() {
   return (
     <div className="flex text-white font-bold font-stretch-condensed md:text-xl py-2">
@@ -13,8 +13,8 @@ export default function Header() {
             <nav className="p-2">
             <ul className="flex gap-4">
               <li className="hover:underline ease-in hover:decoration-blue-600 hover:scale-110 transition"><Link to={"contact"}>Contato</Link></li>
-              <li className="hover:underline ease-inhover:decoration-blue-600 hover:scale-110 transition"><Link to={"about"}>Sobre mim</Link></li>
-              <li className="hover:underline ease-inhover:decoration-blue-600 hover:scale-110 transition"><Link to={"projects"}>Projetos</Link></li>
+              <li className="hover:underline ease-in hover:decoration-blue-600 hover:scale-110 transition"><Link to={"about"}>Sobre mim</Link></li>
+              <li className="hover:underline ease-in hover:decoration-blue-600 hover:scale-110 transition"><Link to={"projects"}>Projetos</Link></li>
             </ul>
             </nav>
         </header>
