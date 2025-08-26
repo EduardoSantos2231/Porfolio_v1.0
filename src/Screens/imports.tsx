4c20d989611c19658d5d3ed0349dd3938420c4ec
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 09:38:37 -0300

    add: criação da página de contato

--- src/Screens/imports.tsx
+++ src/Screens/imports.tsx
@@ -2,5 +2,6 @@ import Hello from "./Hello";
 import NotFound from "./NotFound";
 import Projects from "./Projects";
 import About from "./About";
+import Contact from "./Contact";
 
-export {Hello, NotFound, Projects, About}
+export {Hello, NotFound, Projects, About, Contact}
