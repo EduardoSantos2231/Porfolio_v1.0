0e2d6557c1b30eb239704713f1b32f6b39e85346
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-26 21:13:45 -0300

    add: componentização completa

--- src/Screens/Contact.tsx
+++ src/Screens/Contact.tsx
@@ -1,4 +1,4 @@
-import Descriptional from "@/components/Descriptional";
+
 import GrayBubble from "@/components/GrayBubble";
 import PinkBubble from "@/components/PinkBubble";
 import { FaLinkedin } from "react-icons/fa";
@@ -9,31 +9,14 @@ import ContactCard from "@/components/ContactCard";
 export default function Contact() {
   return (
     <div className="flex w-full gap-3 my-10 flex-col items-center md:justify-center md:gap-5 relative overflow-hidden">
-        <div className="hover:scale-110 transition cursor-pointerw h-fit flex-col">
-          <span className="flex flex-row items-center gap-2">
-          <FaLinkedin size={30}/>
-          <Descriptional title="Linkedin" description=""/>
-          </span>
-         <a href="https://www.linkedin.com/in/edusn/" target="_blank">
-            <p className="underline decoration-blue-400">Conecte-se comigo</p>
-         </a>
-        </div>
+      
+        <ContactCard callTo="Conecte-se comigo" icon={FaLinkedin} link="https://www.linkedin.com/in/edusn/" title="Linkedin"/>
 
         <div className="size-80 bg-radial from-blue-500 opacity-90 to-blue-500 rounded-full float absolute top-10 -left-50 md:-left-44"></div>
 
-        <ContactCard callTo="Veja meus códigos" 
-        icon={FaGithub} 
-        link="https://github.com/EduardoSantos2231" title="Github"/>
+        <ContactCard callTo="Veja meus códigos" icon={FaGithub} link="https://github.com/EduardoSantos2231" title="Github"/>
 
-        <div className="hover:scale-110 transition cursor-pointer h-fit z-1">
-           <span className="flex flex-row gap-2">
-            <CiMail size={30}/>
-            <Descriptional title="Email" description=""/>
-           </span>
-         <a href="mailto:denoticiadose@gmail.com">
-           <p className="underline decoration-blue-400">Entre em contato</p>
-         </a>
-        </div>
+        <ContactCard callTo="Entre em contato" icon={CiMail} link="mailto:denoticiadose@gmail.com" title="Email"/>
         <PinkBubble/>
         <GrayBubble/>
       
