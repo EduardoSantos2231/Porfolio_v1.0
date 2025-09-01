a25a5fe0b0ca1b06b2980af4815f59dac8b30901
Dudu2231 <eduardonascimento2231@gmail.com>
2025-09-01 16:43:55 -0300

    fix: mudar posicionamento bolha rosa

--- src/components/PinkBubble.tsx
+++ src/components/PinkBubble.tsx
@@ -2,6 +2,6 @@
 
 export default function PinkBubble() {
   return (
-    <div className="size-60 top-60 -right-40 absolute rounded-full bg-radial-[at_30%_40%] float from-purple-300 to-purple-400 to-75%"></div>
+    <div className="size-60 bottom-45 -right-40 absolute rounded-full bg-radial-[at_30%_40%] float from-purple-300 to-purple-400 to-75%"></div>
   )
 }
