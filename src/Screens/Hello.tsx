da544424f520ace961b17e2bc62e82ae438b1c2e
Dudu2231 <eduardonascimento2231@gmail.com>
2025-09-30 11:04:53 -0300

    fix: correação divs aninhadas

--- src/Screens/Hello.tsx
+++ src/Screens/Hello.tsx
@@ -8,9 +8,6 @@ export default function Hello() {
   
   return (
     <div className="flex relative items-center justify-center w-full overflow-hidden">
-
-    <div className="z-1 items-center flex flex-col gap-5 md:gap-7 p-3">
-        
         <AnimatedContent
         distance={140}
         direction="vertical"
@@ -22,12 +19,14 @@ export default function Hello() {
         threshold={0.2}
         delay={0.3}>
 
+         
+
         <button className="text-white bg-gradient-to-br p-4 rounded-2xl text-2xl font-extrabold hover:scale-110 transition cursor-pointer purple-glass">
          <Link to={"/about"}>Sobre mim</Link> 
         </button>
 
       </AnimatedContent>
-    </div>
+    
         
     
     </div>
