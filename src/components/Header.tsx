47a9fe46a7e2211ee32ecdf2b0ee9e89d9c0ce55
Dudu2231 <eduardonascimento2231@gmail.com>
2025-09-30 10:24:10 -0300

    add: adicionar efeito de vidro

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 import profile_pic from "@/assets/first_opt.png"
 export default function Header() {
   return (
-    <div className="flex text-white font-bold font-stretch-condensed md:text-xl py-2">
+    <div className="flex text-white font-bold font-stretch-condensed md:text-xl py-2 purple-glass">
         <header className="flex w-full mx-2 items-center">
 
             <div className="mr-auto flex gap-5 items-center">
               <img className="rounded-full w-12 md:w-16" src={profile_pic} alt="" />
-              <h1 className="hidden sm:block underline decoration-blue-600 decoration-double md:text-4xl">Eduardo</h1>
+              <h1 className="hidden sm:block md:text-4xl ">Eduardo</h1>
             </div>
 
             <nav className="p-2">
