48edc15d525660c07c56eef63333a1c2cd20900f
Dudu2231 <eduardonascimento2231@gmail.com>
2025-09-30 21:20:31 -0300

    fix: remover linhas inúteis

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,7 @@ import Hello from "./Screens/Hello";
 export default function Layout() {
   return (
     <div className="min-h-dvh wrap-anywhere relative text-white">
-      <div className="fixed inset-0 z-0 h-dvh"> {/* Adicionei h-dvh para garantir a altura do fundo */}
+      <div className="fixed inset-0 z-0 h-dvh"> 
         <DarkVeil noiseIntensity={0.12} warpAmount={3}/>
       </div>
       <main className="relative z-10 min-h-dvh">
